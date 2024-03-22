a93c2f5e173185332eeb3577557c6e49423d3ae3
Hailekeh22 <hailekjt@gmail.com>
2024-03-22 17:58:36 +0300

    chat desplay section build and media query fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Input from "./Components/Chat/Input";
+import Display from "./Components/Desplay/Display";
 
 function App() {
   return (
     <>
       <Header />
+      <Display />
       <Input />
     </>
   );
---
+++ src/Components/Desplay/Display.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./display.css";
+
+function Display() {
+  return (
+    <div className="displaycontainer">
+      <div className="displaysection"> Display</div>
+    </div>
+  );
+}
+
+export default Display;
