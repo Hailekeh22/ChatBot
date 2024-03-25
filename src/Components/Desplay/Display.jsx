a446af60d0270d3ba6effb56f4137e79fb6349ac
Hailekeh22 <hailekjt@gmail.com>
2024-03-25 18:08:15 +0300

    Finished building the prompt section

--- src/Components/Desplay/Display.jsx
+++ src/Components/Desplay/Display.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./display.css";
+import Prompt from "./Prompt/Prompt";
 
 function Display() {
   return (
     <div className="displaycontainer">
-      <div className="displaysection"> Display</div>
+      <div className="displaysection">
+        <Prompt />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Desplay/Prompt/Prompt.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Prompt.css";
+import usericon from "../../../assets/usericon.png";
+
+function Prompt() {
+  return (
+    <div className="prompttxt">
+      <div className="promptheader">
+        <img className="usericon" src={usericon}></img>
+        <p>You</p>
+      </div>
+      <p className="userprompt">How to make a sandwich</p>
+    </div>
+  );
+}
+
+export default Prompt;
